200ff3f56167e4ff61425f13d1912a15466f1ffe
Abdeldjelil BABAHAMED <8852906+CytrusGallium@users.noreply.github.com>
2022-07-01 07:58:30 +0100

    first

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log("Hello World :)")
+
+const http = require('http');
+const port = process.env.PORT || 5000;
+const uri = process.env.MONGODB_URI;
+
+console.log("URL = " + uri");
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Hello, User!</h1> <h3> Welcome Home</h3>\n');
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on port number::${port}`);
+});
